4dcd95878dc9481884444f99dba53f2d94d5ed73
skoczek06 <maciekrauze@vp.pl>
2024-06-20 18:56:32 +0200

    1.03

--- Main/Quiz_form.php
+++ Main/Quiz_form.php
@@ -2,7 +2,7 @@
 session_start();
 $id_users = $_SESSION['id_users'];
 $quiz_id = $_COOKIE['selected_quiz'];
-
+$points = 0;
 include_once "Connect.php";
 
     $query = "SELECT * FROM quizzes WHERE quiz_id = $quiz_id";
@@ -25,6 +25,8 @@ include_once "Connect.php";
         $questions_text[$i] = $row['question_text'];
         $i++;
     }
+
+        $query2 = "SELECT * FROM quiz_answers WHERE question_id = $questions[$i]";
 }
 
 ?>
@@ -50,10 +52,10 @@ include_once "Connect.php";
         switch ($quiz_type){
             case 1:
                 echo "<h2>QUIZ: $quiz_name</h2>";
+                echo "<form method='POST' action='Quiz_form.php'>";
                 for ($j = 0; $j < count($questions); $j++) {
+                    echo "<div>";
                     echo "<h3>$questions_text[$j]</h3>";
-                    echo "<form method='POST' action='Quiz_form.php'>";
-
                     $query2 = "SELECT * FROM quiz_answers WHERE question_id= $questions[$j]";
                     $result2 = $conn->query($query2);
                     $i = 0;
@@ -61,16 +63,17 @@ include_once "Connect.php";
                     if ($result2->num_rows > 0) {
                         while ($row = $result2->fetch_assoc()) {
                             $answers[$i] = $row['answer_text'];
+
                             echo "<label for=\"html\" >$answers[$i]</label>";
                             echo "<input type='radio' id='$answers[$i]' name = \"$questions[$j]\" value='$questions[$i]'> <br>";
+
                             $i++;
                         }
-                        $submit = "submit" . $questions[$j];
-                        echo "<input type='submit' value='$submit'>";
                     }
-                    echo "</form>";
+                    echo "</div>";
                 }
-
+                echo "<input type='submit' value='submit'>";
+                echo "</form>";
                 break;
             case 2:
 
@@ -80,7 +83,18 @@ include_once "Connect.php";
                 break;
         }
 
-
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        foreach ($questions as $question_id) {
+            // Check if an answer is selected for the current question
+            if (isset($_POST[$question_id])) {
+                $user_answers[$question_id] = $_POST[$question_id];
+            } else {
+                // Handle cases where no answer is selected (optional: assign a default value)
+                $user_answers[$question_id] = null; // or any other default value
+            }
+        }
+    }
+    var_dump($user_answers);
     ?>
 
 
@@ -93,6 +107,8 @@ include_once "Connect.php";
 </body>
 </html>
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
+}
 ?>
 
