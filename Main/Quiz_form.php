a67f373090ae252a7c96870f6131c9849e3cd960
skoczek06 <maciekrauze@vp.pl>
2024-06-23 22:19:40 +0200

    Patch 1.07 - skończone formularze

--- Main/Quiz_form.php
+++ Main/Quiz_form.php
@@ -53,10 +53,10 @@ if ($result1->num_rows > 0) {
     <?php
 
     echo "<h2>QUIZ: $quiz_name</h2>";
-    echo "<form method='POST' action='Quiz_form.php'>";
+
     switch ($quiz_type) {
         case 1:
-
+            echo "<form method='POST' action='Quiz_form.php'>";
             $correct_answers = [];
 
 
@@ -102,121 +102,29 @@ if ($result1->num_rows > 0) {
                 for ($l = 1; $l < count($questions) + 1; $l++) {
 
                     $a = $l-1;
-                $query4 = "INSERT INTO user_quiz_answers (user_id, question_id, selected_answer_id) VALUES ($id_users, $question_id[$a], $user_answers[$l])";
+                $query4 = "INSERT INTO user_quiz_answers (user_id, question_id, selected_answer_id) 
+                            VALUES ($id_users, $questions[$a], $user_answers[$l])";
                 $result4 = $conn->query($query4);
 
-
                     if($user_answers[$l] == $correct_answers[$l] ){
-                   $points++;
+                        $points++;
+                        }
                     }
-                }
+
+
+
                 $query4 = "INSERT INTO user_quiz_attempts (user_id, quiz_id, score) VALUES ($id_users, $quiz_id, $points)";
                 $result4 = $conn->query($query4);
 
-                echo "Qeustions ";
-                print_r($questions);
-                echo " <br> Correct ";
-                print_r($correct_answers);
-                echo " <br>USEr ";
-                print_r( $user_answers);
 
-//        header("Location: QuizList.php");
+                echo "<script>
+                alert('Poprawnie odpowiedziałeś $points razy/raz');
+                window.location.href = 'QuizList.php';
+                </script>";
+                exit();
             }
             break;
         case 2:
-//
-//            $correct_answers = [];
-//
-//
-//            for ($k = 0; $k < count($questions); $k++) {
-//                $query3 = "SELECT * FROM quiz_answers WHERE question_id = $questions[$k] AND is_correct = 1";
-//                $result3 = $conn->query($query3);
-//                $temp_answers = [];
-//                $a = 0;
-//                while ($row = $result3->fetch_assoc()) {
-//                    $temp = $row['answer_id'];
-//                     $temp_answers[$a]= $temp ;
-//                     $a++;
-//                }
-//                $correct_answers[$k] = $temp_answers;
-//            }
-//
-
-
-//            for ($m = 0; $m < count($questions); $m++) {
-//                echo "<div>";
-//                echo "<h3>$questions_text[$m]</h3>";
-//                $query2 = "SELECT * FROM quiz_answers WHERE question_id = $questions[$m]";
-//                $result2 = $conn->query($query2);
-//                $answers = [];
-//                if ($result2->num_rows > 0) {
-//                    while ($row = $result2->fetch_assoc()) {
-//                         $answer_id = $row['answer_id'];
-//                        $answer_text = $row['answer_text'];
-//                        echo "<label for='answer_$answer_id'>$answer_text</label>";
-//                        echo "<input type='checkbox' id='answer_$answer_id' name='question_$questions[$m]' value='$answer_id'> <br>";
-//
-//                    }
-//                }
-//                echo "</div>";
-//            }
-//            echo "<input type='submit' value='submit'>";
-//            echo "</form>";
-//
-//
-//            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//                $user_answers = [];
-//                $a = 1;
-//
-//                for ($n = 0; $n < count($questions); $n++) {
-//                    $temp_answers = $_POST["question_$questions[$n]"];
-//                    var_dump($temp_answers);
-//                    echo "<br>";
-//                    for ($o = 0; $o < count($temp_answers); $o++) {
-//                        $temp_answers[$o] = $_POST["question_$questions[$n]"];
-//                    }
-//                    $user_answers[$n] = $temp_answers;
-//                    var_dump($user_answers);
-//                }
-
-
-//                foreach ($questions as $question_id) {
-//
-//                    var_dump($_POST["question_$answer_id"]);
-//
-//                    if (isset($_POST["question_$question_id"])) {
-//                        $user_answers[$a] = $_POST["question_$question_id"];
-//                    } else {
-//                        $user_answers[$question_id] = null;
-//                    }
-//                    $a++;
-//                }
-
-//                for ($l = 1; $l < count($questions) + 1; $l++) {
-//                    if($user_answers[$l] == $correct_answers[$l] ){
-//                $a = $l-1;
-//                $query4 = "INSERT INTO user_quiz_answers (user_id, question_id, selected_answer_id) VALUES ($id_users, $question_id[$a], $user_answers[$l])";
-//                $result4 = $conn->query($query4);
-//                        $points++;
-////                    }
-//                }
-//                $query4 = "INSERT INTO user_quiz_attempts (user_id, quiz_id, score) VALUES ($id_users, $quiz_id, $points)";
-//                $result4 = $conn->query($query4);
-//
-//                echo "Qeustions ";
-//                print_r($questions);
-//                echo " <br> Correct ";
-//                print_r($correct_answers);
-//                echo " <br>USEr ";
-//                print_r( $user_answers);
-//
-////        header("Location: QuizList.php");
-////            }
-    ///
-    ///
-    ///
-    ///
-
 
             $correct_answers = [];
             for ($k = 0; $k < count($questions); $k++) {
@@ -233,8 +141,7 @@ if ($result1->num_rows > 0) {
                 $stmt3->close();
             }
 
-// Generowanie formularza
-            echo "<form action='' method='POST'>";
+            echo "<form action='Quiz_form.php' method='POST'>";
             for ($m = 0; $m < count($questions); $m++) {
                 echo "<div>";
                 echo "<h3>$questions_text[$m]</h3>";
@@ -257,6 +164,7 @@ if ($result1->num_rows > 0) {
             echo "<input type='submit' value='submit'>";
             echo "</form>";
 
+
             if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $user_answers = [];
                 for ($n = 0; $n < count($questions); $n++) {
@@ -268,13 +176,11 @@ if ($result1->num_rows > 0) {
                     }
                 }
 
-                // Wyświetlenie odpowiedzi dla celów debugowania
-                var_dump($user_answers);
 
                 // Zapis odpowiedzi do bazy danych
                 foreach ($user_answers as $question_id => $answers) {
                     foreach ($answers as $answer_id) {
-                        $query = "INSERT INTO user_quiz_answers (user_id, question_id, answer_id) VALUES (?, ?, ?)";
+                        $query = "INSERT INTO user_quiz_answers (user_id, question_id, selected_answer_id) VALUES (?, ?, ?)";
                         $stmt = $conn->prepare($query);
                         $stmt->bind_param('iii', $id_users, $question_id, $answer_id);
                         $stmt->execute();
@@ -282,61 +188,92 @@ if ($result1->num_rows > 0) {
                 }
 
                 // Sprawdzenie poprawności odpowiedzi
-                $correct_count = 0;
+                $points= 0;
                 foreach ($user_answers as $question_id => $answers) {
                     if (isset($correct_answers[$question_id])) {
                         if ($answers == $correct_answers[$question_id]) {
-                            $correct_count++;
+                            $points++;
                         }
                     }
                 }
-                echo $correct_count;
-                echo "Odpowiedzi zostały zapisane. Liczba poprawnych odpowiedzi: $correct_count";
+                $query4 = "INSERT INTO user_quiz_attempts (user_id, quiz_id, score) VALUES ($id_users, $quiz_id, $points)";
+                $result4 = $conn->query($query4);
+
+                echo "<script>
+        alert('Poprawnie odpowiedziałeś $points razy/raz');
+        window.location.href = 'QuizList.php';
+    </script>";
+                exit();
             }
 
 
-            break;
+
+    break;
         case 3:
-            $correct_answers = [];
+            echo "<form method='POST' action='Quiz_form.php'>";
+            $correct_answers_text = [];
 
-
+// Pobierz poprawne odpowiedzi dla każdego pytania
             for ($k = 0; $k < count($questions); $k++) {
                 $query3 = "SELECT * FROM quiz_answers WHERE question_id = $questions[$k] AND is_correct = 1";
                 $result3 = $conn->query($query3);
                 while ($row = $result3->fetch_assoc()) {
-                    $correct_answers[$k + 1] = $row['answer_id'];
+                    $correct_answers_text[$k] = $row['answer_text']; // Używamy answer_text jako klucza poprawnej odpowiedzi
                 }
             }
-            var_dump($questions_images);
+
+// Wyświetl pytania i pola odpowiedzi w formularzu
             for ($j = 0; $j < count($questions); $j++) {
                 echo "<div>";
                 echo "<h3>$questions_text[$j]</h3>";
-                $query2 = "SELECT * FROM quiz_answers WHERE question_id = $questions[$j]";
-                $result2 = $conn->query($query2);
-                $answers = [];
-                if ($result2->num_rows > 0) {
-                    while ($row = $result2->fetch_assoc()) {
-                        $answer_id = $row['answer_id'];
-                        $answer_text = $row['answer_text'];
-                        echo "<img src=\"$questions_images[$j]\">";
-                        echo "<input type='text' id='answer_$answer_id' > <br>";
-                    }
-                }
+                echo "<img src=\"$questions_images[$j]\">";
+                echo "<input type='text' name='answer_$j' id='answer_$j' required> <br>";
                 echo "</div>";
             }
+
             echo "<input type='submit' value='submit'>";
             echo "</form>";
-            echo "Qeustions ";
-            print_r($questions);
-            echo " <br> Correct ";
-            print_r($correct_answers);
-            echo " <br>USEr ";
-            print_r( $user_answers);
 
+            $points = 0;
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $user_answers = [];
+
+                // Iteruj przez każde pytanie
+                for ($o = 0; $o < count($questions); $o++) {
+                    // Pobierz odpowiedź użytkownika
+                    $user_answer = $_POST["answer_$o"];
+                    $user_answers[] = $user_answer;
+
+                    // Wstaw odpowiedź użytkownika do bazy danych (upewnij się, że dane są bezpieczne!)
+                    $question_id = $questions[$o];
+                    $selected_answer = $conn->real_escape_string($user_answer);
+
+                    var_dump($question_id);
+                    var_dump($selected_answer);
+                    $query4 = "INSERT INTO user_quiz_answers (user_id, question_id, selected_answer_id)
+                   VALUES ($id_users, $question_id, 1)";
+                    $result4 = $conn->query($query4);
+
+                    // Porównaj odpowiedź użytkownika z poprawną odpowiedzią
+                    if ($user_answer == $correct_answers_text[$o]) {
+                        $points++;
+                    }
+                }
+
+                // Wstaw wynik quizu do bazy danych
+                $query5 = "INSERT INTO user_quiz_attempts (user_id, quiz_id, score) VALUES ($id_users, $quiz_id, $points)";
+                $result5 = $conn->query($query5);
+
+                // Wyświetl wynik użytkownikowi
+                echo "<script>
+          alert('Poprawnie odpowiedziałeś $points razy/raz');
+          window.location.href = 'QuizList.php';
+          </script>";
+            }
             break;
 
     }
-
 
     ?>
 </main>
